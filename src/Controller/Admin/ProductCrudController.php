f1b2ed8b5d7e11e4f5a5b2485ec8dc67f003f025
AymenWlf <alinzgohi@gmail.com>
2021-12-01 23:33:37 +0100

    Ajout de se connecter dans registerPage et Gestion du cas pas d'adresses RecapPage

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -35,7 +35,7 @@ class ProductCrudController extends AbstractCrudController
             TextField::new('name','Nom du Produit'),
             SlugField::new('slug','Slug')->setTargetFieldName('name'),
             AssociationField::new('category','Categorie'),
-            TextareaField::new('description','Description'),
+            TextareaField::new('description','Description')->hideOnIndex(),
             BooleanField::new('isBest'),
             TextField::new('subtitle','Sous-Titre'),
             MoneyField::new('price')->setCurrency('MAD'),
